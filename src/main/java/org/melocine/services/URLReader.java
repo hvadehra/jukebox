f9dc57456fa2e3d4d5214cfd516b13da0dff1d26
hvadehra <hvadehra@gmail.com>
2015-06-18 14:44:09 +0530

    fixing azlyrics

--- src/main/java/org/melocine/services/URLReader.java
+++ src/main/java/org/melocine/services/URLReader.java
@@ -3,6 +3,7 @@ package org.melocine.services;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.URL;
+import java.net.URLConnection;
 
 /**
  * Created by IntelliJ IDEA.
@@ -17,8 +18,9 @@ public class URLReader {
         System.err.println("Fetching from: " + url);
         String output = "";
         try{
-            URL oracle = new URL(url);
-            BufferedReader in = new BufferedReader(new InputStreamReader(oracle.openStream()));
+            URLConnection connection = new URL(url).openConnection();
+            connection.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36");
+            BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
             String inputLine;
             while ((inputLine = in.readLine()) != null){
                 output = output + inputLine;
